c0d1eac243866de0395b07696abadc6897b75ba7
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-16 21:02:45 +0000

    CountoTo10 agora invoca CountToN para contar até 10

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -18,10 +18,7 @@ namespace MyFirstMethod
         /// </summary>
         private static void CountTo10()
             {
-                for (int i =1 ; i <= 10; i++)
-                {
-                    Console.WriteLine(i);
-                }
+                CountToN (10);
             }
         private static void CountToN(int n)
         {
