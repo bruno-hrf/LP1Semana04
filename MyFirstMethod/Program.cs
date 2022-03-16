2535ec44fe49d700106f8b82a6bc256a85b32ec3
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-16 20:40:17 +0000

    Fiz summary de CountTo10

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,7 +6,19 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CountTo10();
+            CountTo10();
         }
+
+        /// <summary>
+        /// Função conta de 1 a 10
+        /// </summary>
+        private static void CountTo10()
+            {
+                for (int i =1 ; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
     }
 }
