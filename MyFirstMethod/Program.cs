420fc580319bfd44eb7932185aab09d60b57bc64
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-16 21:54:37 +0000

    recebe 2 numeros diferentes

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,12 +4,18 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string aux = Console.ReadLine();
             int nCount = Convert.ToInt32(aux);
+            string aux2 = Console.ReadLine();
+            int nCount2 = Convert.ToInt32(aux2);
             CountToN(nCount);
-            CountToN(nCount);
+            CountToN(nCount2);
             
         }
 
@@ -22,12 +28,12 @@ namespace MyFirstMethod
             }
 
 
-            /// <summary>
-            /// Função conta de 1 até n
-            /// </summary>
-            /// <param name="n">
-            /// valor inteiro
-            /// </param>
+        /// <summary>
+        /// Função conta de 1 até n
+        /// </summary>
+        /// <param name="n">
+        /// valor inteiro
+        /// </param>
         private static void CountToN(int n)
         {
             for (int i=1 ; i <= n; i ++)
