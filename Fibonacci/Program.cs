c8c1f6e3df0e6cd21913f621997900c8e353c441
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-16 22:22:40 +0000

    Metodo Fibonacci criado

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux = Console.ReadLine();
+            
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            
+            return Fibonacci(n-1) + Fibonacci(n-2);
+
+        }
+
+
+
+
+    }
+}
